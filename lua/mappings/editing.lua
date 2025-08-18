6c2a1275b655336c6c8647548fab84da7192233c
ilusha <ilusha.main@gmail.com>
2025-08-18 20:43:01 +0000

    tmp

--- lua/mappings/editing.lua
+++ lua/mappings/editing.lua
@@ -10,19 +10,30 @@ vim.cmd([[
   nnoremap <S-Tab> <<
 ]])
 
+function remove_word_left() vim.cmd('normal! "_db') end
+function remove_word_right() vim.cmd('normal! "_dw') end
+
 map("n", "<Enter>", "i<Enter><Esc>", { desc = "Insert new line below" })
 map("v", "<Tab>", ">gv", {desc = "indent", noremap = true, silent = true })
 map("v", "<S-Tab>", "<gv", {desc = "unindent", noremap = true, silent = true })
-map('n', '<C-CR>', 'o<Esc>==', { desc = "new line after", noremap = true, silent = true })
-map('i', '<C-CR>', '<Esc>o', { desc = "new line after", noremap = true, silent = true })
+map('n', '<C-Enter>', 'o<Esc>==', { desc = "new line after", noremap = true, silent = true })
+map('i', '<C-Enter>', '<Esc>o', { desc = "new line after", noremap = true, silent = true })
 map("n", "<A-Up>", ":m-2<CR>==", { desc = "Move current line up" })
 map("i", "<A-Up>", "<Esc>:m-2<CR>==gi", { desc = "Move current line up (insert mode)" })
 map("n", "<A-Down>", ":m+<CR>==", { desc = "Move current line down" })
 map("i", "<A-Down>", "<Esc>:m+<CR>==gi", { desc = "Move current line down (insert mode)" })
 map("x", "<M-Up>", ":move '<-2<CR>gv=gv", { desc = "Move selected block up" })
 map("x", "<M-Down>", ":move '>+<CR>gv=gv", { desc = "Move selected block down" })
-map('n', '<BS>', '"_X', { noremap = true, silent = true })
-map('v', '<BS>', '"_d', { noremap = true, silent = true })
+
+
+for _, mode in ipairs({'n', 'v', 'i'}) do
+  map(mode, '<BS>', '"_X', { noremap = true, silent = true })
+  map(mode, '<C-BS>', remove_word_left, { noremap = true, silent = true })
+
+  map(mode, '<Del>', '"_x', { noremap = true, silent = true })
+  map(mode, '<C-Del>', remove_word_right, { noremap = true, silent = true })
+end
+
 map("n", "<C-d>", '"_dd', { noremap = true, silent = true })
 map("n", "<C-v>", '"+P', { desc = "Paste from clipboard" })
 map("n", "<C-x>", 'V"+d', { noremap = true, silent = true })
--- lua/mappings/movement.lua
+++ lua/mappings/movement.lua
@@ -1,10 +1,10 @@
 
 local map = vim.keymap.set
 
-function move_right_word() functions.move_by_sub_word("left") end
-function move_left_word() functions.move_by_sub_word("right") end
-function move_right_sub_word() functions.move_by_word("right") end
-function move_left_sub_word() functions.move_by_word("left") end
+function move_right_word() vim.cmd("normal! w") end
+function move_left_word() vim.cmd("normal! b") end
+function move_right_sub_word() vim.cmd("normal! w") end
+function move_left_sub_word() vim.cmd("normal! b") end
 
 map("n", "<A-Left>", '<C-o>+', { desc = "Cursor prev" })
 map("n", "<A-Right>", '<C-I>+', { desc = "Cursor next" })
@@ -16,7 +16,9 @@ map("n", "<X2Mouse>", '<C-I>+', { desc = "Cursor next" })
 map("i", "<X1Mouse>", '<C-o>+', { desc = "Cursor prev" })
 map("i", "<X2Mouse>", '<C-I>+', { desc = "Cursor next" })
 
-map("n", "<C-A-Right>", move_right_word, { silent = true })
-map("n", "<C-A-Left>",  move_left_word,  { silent = true })
-map("n", "<C-Right>", move_right_sub_word, { silent = true })
-map("n", "<C-Left>", move_left_sub_word, { silent = true })
+for _, mode in ipairs({'n', 'v', 'i'}) do
+  map(mode, "<C-A-Right>", move_right_word, { silent = true })
+  map(mode, "<C-A-Left>", move_left_word, { silent = true })
+  map(mode, "<C-Right>", move_right_sub_word, { silent = true })
+  map(mode, "<C-Left>", move_left_sub_word, { silent = true })
+end
